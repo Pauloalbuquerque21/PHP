43e9c0bac36d0d0cab959fce871ef771d1d9e771
Paulo vitor albuquerque <paulo.vitor21@hotmail.com>
2025-08-22 23:57:08 -0300

    aprendido

---
+++ Estruturas de controle/105_and_108_estrutura_if_else.php
@@ -0,0 +1,16 @@
+<?php
+
+//Variáveis
+$a = 10;
+$b = 5;
+
+$c = "Deu certo, entrou no if 3 <br>";
+$d = "segunda opção";
+
+if($a >= 10){
+    echo $c;
+} else {
+    echo $d;
+}
+
+?>
---
+++ Expressões_operadores/101_Operador_ternario.php
@@ -0,0 +1,14 @@
+<?php
+
+echo 20>10? "Deu true <br>":"Deu false <br>";//Deu true
+
+echo 20<10? "Deu true <br>":"Deu false <br>";//Deu false
+
+$a = 10;
+$b = 5;
+
+echo $a === $b && 10>5 ? "Deu true <br>":"Deu false <br>";
+
+echo $a === $b? "Deu true <br>": "Deu false <br>";
+
+?>
